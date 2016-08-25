5ebef24618b407bc33f412ee475012120a4db43e
SH-Arrow <shurt91112@gmail.com>
2016-08-25 18:21:56 -0400

    //resize window event based code for radial menu position values

--- js/jsFunc.js
+++ js/jsFunc.js
@@ -1,10 +1,43 @@
-// A $( document ).ready() block.
+//before completely loaded, these functions will be activated
 $( document ).ready(function() {
                     $('#btnLearn').addClass("animated fadeInUp");
                     $('.divider').fadeOut(0);
                     $('body').fadeOut(0);
                     });
 
+//on-going listening for resize of window,
+//will then change the size of the radial menu
+
+window.addEventListener("resize", function(){
+                        
+                        var items = document.querySelectorAll('.circle a');
+                        
+                        for(var i = 0, l = items.length; i < l; i++)
+                        {
+                            var x; //affects radius of cos func
+                            var y; //affects radius of sin func
+                        
+                            if ($(window).width() < 768)
+                            {
+                        
+                                x = 3;
+                                y = 3;
+                            }
+                            else if ($(window).width() >= 768)
+                            {
+                                x = 5;
+                                y = 5;
+                            }
+                        
+                            items[i].style.left = (50 - 25*x*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+                        
+                            items[i].style.top = (50 + 15*y*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+                        }
+
+                        
+                        });
+
+//initial js set up for when everything is loaded
 window.onload = function(){
     // radial menu demo by http://creative-punch.net
     
@@ -14,9 +47,24 @@ window.onload = function(){
     
     for(var i = 0, l = items.length; i < l; i++)
     {
-        items[i].style.left = (50 - 125*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        var x; //affects radius of cos func
+        var y; //affects radius of sin func
         
-        items[i].style.top = (50 + 75*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        if ($(window).width() < 768)
+        {
+            
+            x = 3;
+            y = 3;
+        }
+        else if ($(window).width() >= 768)
+        {
+            x = 5;
+            y = 5;
+        }
+        
+        items[i].style.left = (50 - 25*x*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+        
+        items[i].style.top = (50 + 15*y*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
     }
     
     document.querySelector('a.fa-close').onclick = function(e)
