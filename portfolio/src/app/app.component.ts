0b7ffe503f40558813ffe7131ce889999e0f91fc
SH-Arrow <shurt91112@gmail.com>
2017-12-31 19:14:46 -0500

    SH: what is this mess i hate everything

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -14,48 +14,86 @@ export class AppComponent implements OnInit, AfterViewInit {
     subtitle1 = 'Developer. Musician. ';
     subtitle2 = 'Hokage.';
 
-    titleBool:boolean = false;
+    public titleBool:boolean;
+    
+    @ViewChild('box') parallaxBox;
+
+    titleSwitch = function() {  
+        var c1 = document.getElementById ( 'l1' ),
+        c2 = document.getElementById ( 'l2' ), 
+        c3 = document.getElementById( 'nav-btn' ),
+        c4 = c3.getElementsByClassName( 'img' )[0];
+        
+        this.titleBool = !this.titleBool;
+        if(!this.titleBool) {
+            c1.classList.remove('switched');
+            c2.classList.remove('switched');
+            c3.classList.remove('switched');
+            c4.classList.remove('switched');
+        }
+        else {
+            c1.classList.add('switched');
+            c2.classList.add('switched');
+            c3.classList.add('switched');
+            c4.classList.add('switched');
+        }
+    };
 
     ngOnInit() {
         console.log("PARENT INITED");
+        this.titleBool = false;
+//        console.log(this.titleBool);
     }
 
-    ngAfterViewInit() {        
-        var parallaxBox = document.getElementById ( 'box' );
-        var c1left = document.getElementById ( 'l1' ).offsetLeft,
-        c1top = document.getElementById ( 'l1' ).offsetTop,
-        c2left = document.getElementById ( 'l2' ).offsetLeft,
-        c2top = document.getElementById ( 'l2' ).offsetTop;
-        parallaxBox.addEventListener('mousemove', function ( event ) {
-            var x = event.clientX - parallaxBox.offsetLeft,
-            y = event.clientY - parallaxBox.offsetTop;
-
-            mouseParallax ( 'l1', c1left, c1top, x, y, 14 );
-            mouseParallax ( 'l2', c2left, c2top, x, y, 6 );
-        })
-
-        function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
-            if(mobilecheck()) {
-               return
-            }
-               
-            var obj = document.getElementById ( id );
-            var parentObj = parallaxBox;
-            
-            var containerWidth = parseInt( parentObj.offsetWidth.toString() ),
-            containerHeight = parseInt( parentObj.offsetHeight.toString() );
-            obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth.toString() ) / 2 + left ) ) / containerWidth ) * speed ) + 'px';
-            obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight.toString() ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
+    mouseParallax = function(id, left, top, mouseX, mouseY, speed ) {
+        if(mobilecheck()) {
+           return
         }
+
+        if(this.titleBool) {
+            return
+        }
+
+        var obj = document.getElementById ( id );
+//            
+//            var cont = obj.classList.contains('switched');
+//            if(cont) {
+//                return;
+//            }
+//            
+        var parentObj = this.parallaxBox.nativeElement;
+
+        var containerWidth = parseInt( parentObj.offsetWidth.toString() ),
+        containerHeight = parseInt( parentObj.offsetHeight.toString() );
+        obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth.toString() ) / 2 + left ) ) / containerWidth ) * speed ) + 'px';
+        obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight.toString() ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
+    }
         
         //mobile check
-        function mobilecheck() {
+    mobilecheck = function() {
           var check = false;
           (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor);
           return check;
         };
+
+    mouseMove = function ( event ) {
+            var parallaxBox = document.getElementById ( 'box' );
+            var x = event.clientX - parallaxBox.offsetLeft,
+            y = event.clientY - parallaxBox.offsetTop;
+
+            this.mouseParallax ( 'l1', c1left, c1top, x, y, 14 );
+            this.mouseParallax ( 'l2', c2left, c2top, x, y, 6 );
+        }
+
+    ngAfterViewInit() { 
         
-        parallaxBox.classList.add('visible');
+        var c1left = document.getElementById ( 'l1' ).offsetLeft,
+        c1top = document.getElementById ( 'l1' ).offsetTop,
+        c2left = document.getElementById ( 'l2' ).offsetLeft,
+        c2top = document.getElementById ( 'l2' ).offsetTop;
+        this.parallaxBox.nativeElement.addEventListener('mousemove', this.mouseMove);
+//        console.log(this.parallaxBox);
+        this.parallaxBox.nativeElement.classList.add('visible');
         document.getElementById ( 'nav-btn' ).classList.add('visible');
     }
 }
