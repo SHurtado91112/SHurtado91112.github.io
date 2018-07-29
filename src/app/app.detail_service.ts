b83ee24263905224bd48cf8f45668e9103fd303c
SH-Arrow <shurt91112@gmail.com>
2018-07-29 15:28:23 -0700

    SH: moving to dist for build

--- src/app/app.detail_service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class DetailService {
-  public content: any;    
-  constructor() {
-      console.log('DetailService initted');
-  }
-}
